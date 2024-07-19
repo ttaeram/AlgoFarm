1f66f715e003edb3964edfc179fd12b032816ffe
hoiae <donald819@naver.com>
2024-07-19 16:16:36 +0900

    fix: Too Many Redirects

--- back/src/main/java/com/ssafy/algoFarm/algo/controller/HomeController.java
+++ back/src/main/java/com/ssafy/algoFarm/algo/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.ssafy.algoFarm.algo.test;
+package com.ssafy.algoFarm.algo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
