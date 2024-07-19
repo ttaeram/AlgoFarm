1c406da367b82c5c84bfb2ea14ee7c1057c6f1d9
hoiae <donald819@naver.com>
2024-07-19 16:47:20 +0900

    refactor: rename class from jenkinsTestApplication to AlogoFarmApplication

---
+++ back/src/main/java/com/ssafy/AlgoFarmApplication.java
@@ -0,0 +1,11 @@
+package com.ssafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoFarmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoFarmApplication.class, args);
+    }
+}
