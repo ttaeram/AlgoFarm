db0263cf4d8158461e787f73a3d232b4f464c508
박정영 <bjy123bjy@gmail.com>
2024-07-22 14:14:30 +0900

    feat : OAuth 기능 추가,  - 로그인을 할 때 기존 회원이면 바로 로그인  - 기존 회원이 아니면 회원가입
    [추후 방향]
     - JWT를 이용한 인증 기능 구현 예정

--- back/src/main/java/com/ssafy/algoFarm/algo/auth/JwtAuthenticationFilter.java
+++ back/src/main/java/com/ssafy/algoFarm/algo/auth/JwtAuthenticationFilter.java
@@ -1,4 +1,6 @@
-package org.example.algo.auth;
+package com.ssafy.algoFarm.algo.auth;
+
+import com.ssafy.algoFarm.algo.auth.JwtUtil;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
--- back/src/test/java/org/example/algo/JenkinsTestApplicationTests.java
+++ back/src/test/java/org/example/algo/JenkinsTestApplicationTests.java
@@ -1,16 +1,11 @@
 package org.example.algo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@EntityScan("org.example.jenkinstest.Auth")
 
 class JenkinsTestApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
