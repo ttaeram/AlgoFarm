04f15094a5e09b7e1fcdd36abf3ffb505db989d1
박정영 <bjy123bjy@gmail.com>
2024-07-22 17:57:36 +0900

    커ë°개발용 Oauth 확인하기

--- back/src/main/java/com/ssafy/global/swagger/SwaggerConfig.java
+++ back/src/main/java/com/ssafy/global/swagger/SwaggerConfig.java
@@ -2,20 +2,33 @@ package com.ssafy.global.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.*;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                )
+                .security(Collections.singletonList(new SecurityRequirement().addList("bearer-jwt")))
                 .info(new Info().title("AlgoFarm API")
-                        .description("AlgoFarm application API documentation")
+                        .description("AlgoFarm application API documentation. Use the Authorize button to input your JWT token.")
                         .version("v1.0.0"));
     }
+
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
