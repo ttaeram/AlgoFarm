16d90f49f19e5c17a85240daccf01ac13a22de0d
SSAFYDKL <akhack1231@skuniv.ac.kr>
2024-07-22 13:06:51 +0900

    FEAT: Create swagger docs

--- back/src/main/java/com/ssafy/algoFarm/chat/ChatController.java
+++ back/src/main/java/com/ssafy/algoFarm/chat/ChatController.java
@@ -1,6 +1,7 @@
 package com.ssafy.algoFarm.chat;
 
 import com.ssafy.algoFarm.chat.entity.ChatMessage;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +26,7 @@ public class ChatController {
      * @return ResponseEntity<String>
      */
     @PostMapping("/{chatroomId}/join")
+    @Operation(summary="Enter Chatroom", description="API used to include a user in a chat room")
     public ResponseEntity<String> join(@PathVariable String chatroomId, @RequestBody ChatMessage chatMessage) {
         messagingTemplate.convertAndSend("/chat/" + chatroomId, chatMessage);
         return ResponseEntity.ok("Joined chatroomId " + chatroomId);
@@ -36,6 +38,7 @@ public class ChatController {
      * @return 최근 30일간 채팅 리스트
      */
     @GetMapping("/{chatroomId}/all")
+    @Operation(summary="Getting Chatlist", description = "API to get chat lists for a certain chat room")
     public ResponseEntity<List<ChatMessage>> getAll(@PathVariable String chatroomId) {
         return ResponseEntity.ok(chatService.getAllChatMessages(Long.parseLong(chatroomId)));
     }
@@ -47,6 +50,7 @@ public class ChatController {
      * @return ResponseEntity<String>
      */
     @PostMapping("/{chatroomId}/send")
+    @Operation(summary = "Send message on chatroom", description = "API to send chat for a certain chat room")
     public ResponseEntity<String> send(@PathVariable String chatroomId, @RequestBody ChatMessage chatMessage) {
         System.out.println(chatMessage.toString());
 
