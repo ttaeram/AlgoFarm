0f345dbb05e2a84e47ace5b6b80dfca6df9df0e5
SSAFYDKL <akhack1231@skuniv.ac.kr>
2024-07-23 17:03:31 +0900

    REFACT: create ChatMessageResDTO, ChatMessageReqDTO and refact structure of chatAPI

---
+++ back/src/main/java/com/ssafy/algoFarm/chat/entity/ChatMessageResDTO.java
@@ -0,0 +1,18 @@
+package com.ssafy.algoFarm.chat.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatMessageResDTO {
+    private String nickname;
+    private String content;
+    private LocalDateTime createAt;
+}
