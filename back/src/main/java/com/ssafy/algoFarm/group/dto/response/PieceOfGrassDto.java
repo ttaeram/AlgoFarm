37b7c206123b1cb943a1f5d547422784170179b4
qnsghdharo119 <qnsghdharo119@naver.com>
2024-07-30 17:33:37 +0900

    BE | fix : final dto setting

--- back/src/main/java/com/ssafy/algoFarm/group/dto/response/PieceOfGrassDto.java
+++ back/src/main/java/com/ssafy/algoFarm/group/dto/response/PieceOfGrassDto.java
@@ -6,22 +6,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
 @AllArgsConstructor
 @Schema(description = "그룹 잔디 데이터의 하루 치 정보를 담은 DTO")
 @Getter
 public class PieceOfGrassDto {
 
     @Schema(description = "잔디 한 포기에 해당하는 날짜데이터")
-    private String timestamp;
+    private final String timestamp;
 
     @Schema(description = "해당 일자에 문제를 푼 그룹원의 수")
-    private int commitCount;
+    private final int commitCount;
 
     @Schema(description = "잔디 한 포기에 대한 주차별 정보")
-    private int x;
+    private final int x;
 
     @Schema(description = "잔디 한 포기에 대한 요일별 정보")
-    private int y;
+    private final int y;
 
 }
