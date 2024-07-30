f4951ade0f14431788e7c3b97c478256c697e8c1
qnsghdharo119 <qnsghdharo119@naver.com>
2024-07-30 16:24:25 +0900

    BE | feat : grassdata api add

---
+++ back/src/main/java/com/ssafy/algoFarm/group/dto/response/PieceOfGrassDto.java
@@ -0,0 +1,28 @@
+package com.ssafy.algoFarm.group.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "그룹 잔디 데이터의 하루 치 정보를 담은 DTO")
+@Getter
+@Setter
+public class PieceOfGrassDto {
+
+    @Schema(description = "잔디 한 포기에 해당하는 날짜데이터")
+    private String timestamp;
+
+    @Schema(description = "해당 일자에 문제를 푼 그룹원의 수")
+    private int commitCount;
+
+    @Schema(description = "잔디 한 포기에 대한 주차별 정보")
+    private int x;
+
+    @Schema(description = "잔디 한 포기에 대한 요일별 정보")
+    private int y;
+
+}
--- back/src/main/java/com/ssafy/algoFarm/solution/repository/AlgorithmSolutionRepository.java
+++ back/src/main/java/com/ssafy/algoFarm/solution/repository/AlgorithmSolutionRepository.java
@@ -3,9 +3,19 @@ package com.ssafy.algoFarm.solution.repository;
 import com.ssafy.algoFarm.algo.user.entity.User;
 import com.ssafy.algoFarm.solution.entity.AlgorithmSolution;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AlgorithmSolutionRepository extends JpaRepository<AlgorithmSolution, Long> {
     Optional<AlgorithmSolution> findByUserIdAndProblemId(Long userId, Long problemId);
+
+    @Query(value = "SELECT DATE(s.current_datet_time) as date, COUNT(DISTINCT s.user_id) as commitCount " +
+            "FROM algorithm_solution s " +
+            "JOIN member m ON s.user_id = m.user_id " +
+            "WHERE m.group_id = :groupId " +
+            "GROUP BY DATE(s.current_datet_time)", nativeQuery = true)
+    List<Object[]> findCommitCountByGroupId(@Param("groupId") Long groupId);
 }
