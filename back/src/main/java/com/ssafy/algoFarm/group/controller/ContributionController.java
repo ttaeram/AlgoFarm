c82a18401ab41b8d2b99181bbfb8db6648405a54
qnsghdharo119 <qnsghdharo119@naver.com>
2024-08-01 12:30:42 +0900

    BE|fix:submitTime fixed

--- back/src/main/java/com/ssafy/algoFarm/group/controller/ContributionController.java
+++ back/src/main/java/com/ssafy/algoFarm/group/controller/ContributionController.java
@@ -1,4 +1,21 @@
 package com.ssafy.algoFarm.group.controller;
 
+import com.ssafy.algoFarm.group.service.ContributionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+@Slf4j
 public class ContributionController {
+    private final ContributionService contributionService;
+
+    @Autowired
+    public ContributionController(ContributionService contributionService) {
+        this.contributionService = contributionService;
+    }
 }
