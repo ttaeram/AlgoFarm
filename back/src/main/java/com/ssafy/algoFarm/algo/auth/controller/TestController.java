aa0ae0271228f0b6681fd370fee814053a042162
박정영 <bjy123bjy@gmail.com>
2024-08-01 14:24:51 +0900

    update : Swagger
    Swagger UI 페이지에서 https 를 사용할 수 있게 수정했습니다.

--- back/src/main/java/com/ssafy/algoFarm/algo/auth/controller/TestController.java
+++ back/src/main/java/com/ssafy/algoFarm/algo/auth/controller/TestController.java
@@ -2,6 +2,7 @@ package com.ssafy.algoFarm.algo.auth.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -26,5 +27,16 @@ public class TestController {
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증되지 않은 사용자입니다.");
         }
+    }
+    @GetMapping("/debug/server-info")
+    public String getServerInfo(HttpServletRequest request) {
+        StringBuilder info = new StringBuilder();
+        info.append("Server Name: ").append(request.getServerName()).append("\n");
+        info.append("Server Port: ").append(request.getServerPort()).append("\n");
+        info.append("Scheme: ").append(request.getScheme()).append("\n");
+        info.append("X-Forwarded-Proto: ").append(request.getHeader("X-Forwarded-Proto")).append("\n");
+        info.append("X-Forwarded-Host: ").append(request.getHeader("X-Forwarded-Host")).append("\n");
+        info.append("X-Forwarded-Port: ").append(request.getHeader("X-Forwarded-Port")).append("\n");
+        return info.toString();
     }
 }
