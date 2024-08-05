beb367387d6ec003683774204b5691639629aa11
SSAFYDKL <akhack1231@skuniv.ac.kr>
2024-08-05 16:22:32 +0900

    BE/FEAT: Create scheduler delete over 30days ChatMessage

--- back/src/main/java/com/ssafy/AlgoFarmApplication.java
+++ back/src/main/java/com/ssafy/AlgoFarmApplication.java
@@ -1,8 +1,10 @@
 package com.ssafy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AlgoFarmApplication {
     public static void main(String[] args) {
         SpringApplication.run(AlgoFarmApplication.class, args);
--- back/src/main/java/com/ssafy/algoFarm/chat/repository/ChatMessageRepository.java
+++ back/src/main/java/com/ssafy/algoFarm/chat/repository/ChatMessageRepository.java
@@ -2,11 +2,18 @@ package com.ssafy.algoFarm.chat.repository;
 
 import com.ssafy.algoFarm.chat.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
     List<ChatMessage> findByGroupId(long groupId);
+
+    @Modifying
+    @Query("DELETE FROM ChatMessage c WHERE c.createAt < :cutoffDate")
+    void deleteMessagesOlderThan(LocalDateTime cutoffDate);
 }
