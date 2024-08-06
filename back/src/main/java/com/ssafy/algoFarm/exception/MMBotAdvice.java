4c674dee45d82346dc9c79a0c91485d037a43325
박정영 <bjy123bjy@gmail.com>
2024-08-06 17:42:54 +0900

    feat: 예외 처리 및 알림 기능 개선
    - 로컬 프로파일에서 예외 발생 시 콘솔에 스택 트레이스 출력
    - 프로덕션 프로파일에서 예외 발생 시 Mattermost로 알림 전송
    - 코드 가독성을 높이기 위해 메서드 분리 및 주석 추가

--- back/src/main/java/com/ssafy/algoFarm/exception/MMBotAdvice.java
+++ back/src/main/java/com/ssafy/algoFarm/exception/MMBotAdvice.java
@@ -1,6 +1,10 @@
 package com.ssafy.algoFarm.exception;
 
 import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,47 +20,85 @@ import java.util.Map;
 @RestControllerAdvice
 public class MMBotAdvice {
 
+    @Autowired
+    private Environment env;
+
     @ExceptionHandler(Exception.class)
-    public String exception(Exception e) throws IOException {
-        URL url = new URL("https://meeting.ssafy.com/hooks/wm77yto8kfd98mudd9z6pm51ca");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    public ResponseEntity<String> handleException(Exception e) throws IOException {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        String responseBody;
 
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("Content-Type", "application/json");
-        conn.setDoOutput(true);
+        if (isLocalProfileActive()) {
+            // 로컬 프로파일인 경우 예외 스택을 콘솔에 출력
+            e.printStackTrace();
+            responseBody = "Exception occurred in local profile: " + e.getMessage();
+        } else if (isProdProfileActive()) {
+            // prod 프로파일인 경우 MM으로 전송
+            String message = formatExceptionMessage(e);
+            sendToMM(message);
+            responseBody = "Exception occurred in prod profile. Details sent to MM.";
+        } else {
+            // 다른 프로파일의 경우 기본 처리
+            responseBody = "Exception occurred: " + e.getMessage();
+        }
 
-        Map<String, String> message = new HashMap<>();
+        return new ResponseEntity<>(responseBody, status);
+    }
 
+    private boolean isLocalProfileActive() {
+        return isProfileActive("local");
+    }
+
+    private boolean isProdProfileActive() {
+        return isProfileActive("prod");
+    }
+
+    private boolean isProfileActive(String profileName) {
+        String[] activeProfiles = env.getActiveProfiles();
+        for (String profile : activeProfiles) {
+            if (profileName.equals(profile)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String formatExceptionMessage(Exception e) {
         StringBuilder sb = new StringBuilder();
         sb.append("### ").append(e.toString()).append("\n\n\n").append("```\n");
 
-        StackTraceElement[] steList = e.getStackTrace();
-
-        for(StackTraceElement ste : steList) {
-            if(ste.toString().contains("com.ssafy")){
+        for (StackTraceElement ste : e.getStackTrace()) {
+            if (ste.toString().contains("com.ssafy")) {
                 sb.append("[ !!! ] ");
             }
             sb.append(ste.toString()).append("\n");
         }
         sb.append("\n```");
-        message.put("text", sb.toString());
 
-        String jsonString = new Gson().toJson(message);
+        Map<String, String> message = new HashMap<>();
+        message.put("text", sb.toString());
+        return new Gson().toJson(message);
+    }
+
+    private void sendToMM(String message) throws IOException {
+        URL url = new URL("https://meeting.ssafy.com/hooks/wm77yto8kfd98mudd9z6pm51ca");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setDoOutput(true);
 
         try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream(), StandardCharsets.UTF_8))) {
-            writer.write(jsonString);
+            writer.write(message);
             writer.flush();
         }
 
         int responseCode = conn.getResponseCode();
         conn.disconnect();
 
-        if (responseCode != 200) {
-            System.err.println("HTTP Request failed with response code " + responseCode);
+        if (responseCode != HttpStatus.OK.value()) {
+            System.err.println("HTTP Request to MM failed with response code " + responseCode);
         } else {
-            System.out.println("Message sent successfully");
+            System.out.println("Message sent successfully to MM");
         }
-
-        return jsonString;
     }
 }
