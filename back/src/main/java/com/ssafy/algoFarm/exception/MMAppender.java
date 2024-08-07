67981679d54170500d12371652a13494a23dfe86
박정영 <bjy123bjy@gmail.com>
2024-08-07 10:03:49 +0900

    fix : MMAppender.java 에서 의존성 문제 수정 발생 원인 MMAppender 클래스: @Component 어노테이션이 사용 이는 Spring의 빈으로 관리되도록 하는 것 생성자에 Environment 파라미터 -> Spring의 의존성 주입을 기대하는 것
    logback-spring.xml:
    
    MMAppender를 일반적인 Logback Appender로 구성

--- back/src/main/java/com/ssafy/algoFarm/exception/MMAppender.java
+++ back/src/main/java/com/ssafy/algoFarm/exception/MMAppender.java
@@ -1,9 +1,12 @@
 package com.ssafy.algoFarm.exception;
 
+import ch.qos.logback.classic.spi.StackTraceElementProxy;
 import ch.qos.logback.core.AppenderBase;
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.IThrowableProxy;
 import com.google.gson.Gson;
-import lombok.NoArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -14,14 +17,18 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
-@NoArgsConstructor
+@Component
 public class MMAppender extends AppenderBase<ILoggingEvent> {
     private static final String WEBHOOK_URL = "https://meeting.ssafy.com/hooks/wm77yto8kfd98mudd9z6pm51ca";
+    private final Environment env;
 
+    public MMAppender(Environment env) {
+        this.env = env;
+    }
 
     @Override
     protected void append(ILoggingEvent event) {
-        if (event.getLevel().isGreaterOrEqual(ch.qos.logback.classic.Level.ERROR)) {
+        if (isProdProfile() && isErrorOrException(event)) {
             String message = formatLogMessage(event);
             try {
                 sendToMM(message);
@@ -31,11 +38,36 @@ public class MMAppender extends AppenderBase<ILoggingEvent> {
         }
     }
 
+    private boolean isProdProfile() {
+        String[] activeProfiles = env.getActiveProfiles();
+        for (String profile : activeProfiles) {
+            if ("prod".equals(profile)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isErrorOrException(ILoggingEvent event) {
+        return event.getLevel().isGreaterOrEqual(ch.qos.logback.classic.Level.ERROR) || event.getThrowableProxy() != null;
+    }
+
     private String formatLogMessage(ILoggingEvent event) {
         StringBuilder sb = new StringBuilder();
-        sb.append("### ").append(event.getLevel()).append(": ").append(event.getLoggerName()).append("\n\n\n").append("```\n");
+        sb.append("### ").append(event.getLevel()).append(": ").append(event.getLoggerName()).append("\n\n");
+        sb.append("```\n");
         sb.append(event.getFormattedMessage()).append("\n");
-        sb.append("\n```");
+
+        IThrowableProxy throwableProxy = event.getThrowableProxy();
+        if (throwableProxy != null) {
+            sb.append("\nException stack trace:\n");
+            sb.append(throwableProxy.getClassName()).append(": ").append(throwableProxy.getMessage()).append("\n");
+            for (StackTraceElementProxy ste : throwableProxy.getStackTraceElementProxyArray()) {
+                sb.append("\tat ").append(ste.toString()).append("\n");
+            }
+        }
+
+        sb.append("```");
 
         Map<String, String> message = new HashMap<>();
         message.put("text", sb.toString());
@@ -60,7 +92,7 @@ public class MMAppender extends AppenderBase<ILoggingEvent> {
         if (responseCode != 200) {
             System.err.println("HTTP Request failed with response code " + responseCode);
         } else {
-            System.out.println("Message sent successfully");
+            System.out.println("Message sent successfully to Mattermost");
         }
     }
 }
