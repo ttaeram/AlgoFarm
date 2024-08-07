3fd9df48e9d2e87c4a5aac8bfa78e3905ac0591b
hoiae <donald819@naver.com>
2024-08-07 17:51:45 +0900

    style: createGroup Page

--- front/src/popup/pages/CreateGroup.js
+++ front/src/popup/pages/CreateGroup.js
@@ -1,14 +1,14 @@
-import { useState } from 'react';
-import { useAuth } from '../context/context';
-import BackButton from '../components/BackButton';  // BackButton 컴포넌트 import
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import * as styles from "./CreateGroup.module.css";
+import { useAuth } from '../context/context';
+import BackButton from '../components/BackButton';
+import { Box, Typography, Button, TextField, Alert } from '@mui/material';
 
 function CreateGroup() {
   const [groupName, setGroupName] = useState('');
   const [showWarning, setShowWarning] = useState(false);
   const { jwt, setGroupId, fetchGroupInfo } = useAuth();
-  const navigate = useNavigate();  // useNavigate 훅을 사용
+  const navigate = useNavigate();
 
   const handleInputChange = (e) => {
     setGroupName(e.target.value);
@@ -20,7 +20,7 @@ function CreateGroup() {
       setShowWarning(true);
       setTimeout(() => {
         setShowWarning(false);
-      }, 3000); // 3초 후에 문구 숨기기
+      }, 3000);
     } else {
       try {
         const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/api/groups`, {
@@ -38,45 +38,114 @@ function CreateGroup() {
 
         const data = await response.json();
         const newGroupId = data.data.groupId;
-        setGroupId(newGroupId); // 그룹 ID를 설정
+        setGroupId(newGroupId);
 
-        // 그룹 정보를 가져와서 Context에 저장
         await fetchGroupInfo(jwt, newGroupId);
 
-        navigate('/my-page/group-info');  // 그룹 생성이 성공하면 MyPage로 이동
+        navigate('/my-page/group-info');
       } catch (error) {
         console.error('Error creating group:', error);
         setShowWarning(true);
         setTimeout(() => {
           setShowWarning(false);
-        }, 3000); // 3초 후에 문구 숨기기
+        }, 3000);
       }
     }
   };
 
   return (
-    <div className={styles.createGroup}>
-      <BackButton />
-      <h1 className={styles.title}>알고팜</h1>
-      <div className={styles.algoFarm}>
-        <img src="" alt="algoFarm" />
-      </div>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <label className={styles.label} htmlFor="groupName">그룹 이름</label>
-        <input
-          className={styles.input}
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 500,
+        height: 500,
+        p: 2,
+        backgroundColor: '#f5f5f5',
+        borderRadius: 2,
+        boxShadow: 3,
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+          width: '100%',
+          mb: 3,
+        }}
+      >
+        <BackButton/>
+          <Typography
+            variant="h4"
+            sx={{ ml: 15, fontWeight: 'bold', color: '#4caf50', textAlign: 'center'}}
+          >
+            알고팜
+          </Typography>
+      </Box>
+
+      <Box
+        sx={{
+          width: 340,
+          height: 210,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          mb: 3,
+          overflow: 'hidden',
+          backgroundColor: 'white',
+          borderRadius: 2,
+          boxShadow: 1,
+        }}
+      >
+        <img
+          src="images/logo.jpeg"
+          alt="algoFarm"
+          style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }}
+        />
+      </Box>
+
+      <form onSubmit={handleSubmit} style={{ width: '340px' }}>
+      <Box
+          display="flex"
+          alignItems="center"
+          mb={2}
+          sx={{ width: '340px' }}
+        >
+        <Typography variant="h6" sx={{ mr: 2 , whiteSpace: 'nowrap'}}>
+          그룹 이름
+        </Typography>
+      <Box display="flex" alignItems="center" flexGrow={1}>
+        <TextField
           type="text"
-          id="groupName"
-          placeholder="그룹 이름"
           value={groupName}
           onChange={handleInputChange}
+          placeholder="생성할 그룹 이름을 입력하세요"
+          variant="outlined"
+          fullWidth
+          sx={{ mb: 2 }}
         />
-        <button className={styles.button} type="submit">그룹 생성</button>
-        <div className={styles.warningContainer}>
-          {showWarning && <div className={styles.warning}>그룹 이름을 입력하세요.</div>}
-        </div>
+        </Box>
+        </Box>
+
+        <Button
+          variant="contained"
+          color="success"
+          type="submit"
+          fullWidth
+          sx={{ mb: 2 }}
+        >
+          그룹 생성
+        </Button>
+        {showWarning && (
+          <Alert severity="warning" sx={{ mb: 2 }}>
+            그룹 이름을 입력하세요.
+          </Alert>
+        )}
       </form>
-    </div>
+    </Box>
   );
 }
 
