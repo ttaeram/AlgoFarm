562ab5055df4fff990eb6676187253c835c96e31
ttaeram <ryu.taeram@gmail.com>
2024-08-07 13:19:36 +0900

    FE | fix: group name

--- front/src/popup/components/Navbar.js
+++ front/src/popup/components/Navbar.js
@@ -4,28 +4,27 @@ import * as styles from './Navbar.module.css';
 function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <NavLink to="group-info" className={styles.button}>
-        <svg
-          class="icon"
-          stroke="currentColor"
-          fill="none"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          aria-hidden="true"
-          height="1em"
-          width="1em"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          ></path>
-        </svg>
-      </NavLink>
-      <NavLink to="member-info" className={({ isActive }) => (isActive ? styles.active : undefined)}>그룹원 정보</NavLink>
-      <NavLink to="member-manage" className={({ isActive }) => (isActive ? styles.active : undefined)}>그룹원 관리</NavLink>
-      <NavLink to="settings" className={({ isActive }) => (isActive ? styles.active : undefined)}>더보기...</NavLink>
+      <input type="radio" name="tab" id="tab1" className={styles.tab_1} defaultChecked />
+      <label className={styles.tab_label} htmlFor="tab1">
+        <NavLink to="group-info">Group Info</NavLink>
+      </label>
+      
+      <input type="radio" name="tab" id="tab2" className={styles.tab_2} />
+      <label className={styles.tab_label} htmlFor="tab2">
+        <NavLink to="member-info">Member Info</NavLink>
+      </label>
+      
+      <input type="radio" name="tab" id="tab3" className={styles.tab_3} />
+      <label className={styles.tab_label} htmlFor="tab3">
+        <NavLink to="member-manage">Member Manage</NavLink>
+      </label>
+      
+      <input type="radio" name="tab" id="tab4" className={styles.tab_4} />
+      <label className={styles.tab_label} htmlFor="tab4">
+        <NavLink to="settings">Settings</NavLink>
+      </label>
+      
+      <div className={styles.indicator}></div>
     </nav>
   );
 }
