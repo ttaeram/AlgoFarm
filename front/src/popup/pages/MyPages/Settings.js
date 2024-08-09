505d3e3c1bf6c9225ea875a98b94b437255a385b
박정영 <bjy123bjy@gmail.com>
2024-08-09 09:29:38 +0900

    fix : 캐릭터 모델 뷰어가 업데이트 되면서 캐릭터와 관련된 모든 코드를 업데이트

--- front/src/popup/pages/MyPages/Settings.js
+++ front/src/popup/pages/MyPages/Settings.js
@@ -6,7 +6,6 @@ import ToggleEnableButton from '../../components/ToggleEnableButton';
 import { TextField, Button, Box, Typography } from '@mui/material';
 import ToggleCharacterButton from '../../components/ToggleCharacterButton';
 import { styled } from '@mui/system';
-import ToggleCharacterButton from '../../components/ToggleCharacterButton';
 import * as styles from './settings.module.css';
 
 const Container = styled(Box)`
