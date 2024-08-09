d84d84e9fa0d86b017402e776ac5d23482c9be78
SSAFYDKL <akhack1231@skuniv.ac.kr>
2024-08-09 10:33:01 +0900

    BE/FIX: fix getting userInfo

--- back/src/main/java/com/ssafy/algoFarm/algo/user/UserInfo.java
+++ back/src/main/java/com/ssafy/algoFarm/algo/user/UserInfo.java
@@ -8,12 +8,9 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class UserInfo {
-    private String id;
+    private Long user_id;
     private String email;
-    private boolean verified_email;
+    private boolean email_verified;
     private String name;
-    private String given_name;
-    private String family_name;
-    private String picture;
-    private String locale;
+    private String provider;
 }
