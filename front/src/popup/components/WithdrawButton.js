91c9c954347ef700c5765c8b94bfbf139c8900a9
ttaeram <ryu.taeram@gmail.com>
2024-08-13 17:00:15 +0900

    FE | style: Withdraw, GroupLeave

--- front/src/popup/components/WithdrawButton.js
+++ front/src/popup/components/WithdrawButton.js
@@ -1,11 +1,38 @@
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from "../context/context";
-import { Button } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import { styled } from '@mui/system';
 import * as styles from './WithdrawButton.module.css';
+
+const CustomCancelButton = styled(Button)({
+  backgroundColor: '#ffffff',
+  color: '#FD88A0',
+  '&:hover': {
+    backgroundColor: '#FCE6E0',
+  },
+});
+
+const CustomWithdrawButton = styled(Button)({
+  backgroundColor: '#FD88A0',
+  color: '#ffffff',
+  '&:hover': {
+    backgroundColor: '#EB4A52',
+  },
+});
 
 const WithdrawButton = () => {
   const { setIsLogined, jwt } = useAuth();
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const handleWithdraw = async () => {
     try {
@@ -21,23 +48,47 @@ const WithdrawButton = () => {
         setIsLogined(false);
         navigate('/');
       } else {
-        console.log("회원탈퇴 중 오류가 발생했습니다:", response.status)
+        console.log("회원탈퇴 중 오류가 발생했습니다:", response.status);
       }
     } catch (error) {
       console.log("회원탈퇴 중 네트워크 오류가 발생했습니다:", error);
+    } finally {
+      handleClose(); // 팝업 닫기
     }
   };
 
   return (
-    <Button
-      className={styles.WithdrawButton}
-      onClick={handleWithdraw}
-      variant="contained"
-      style={{ backgroundColor: '#eb647e' }}  // style 속성을 사용하여 배경색 지정
-    >
-      회원탈퇴
-    </Button>
-  );
-}
+    <>
+      <Button
+        className={styles.WithdrawButton}
+        onClick={handleOpen}
+        variant="contained"
+        style={{ backgroundColor: '#eb647e' }}  // style 속성을 사용하여 배경색 지정
+      >
+        회원탈퇴
+      </Button>
 
-export default WithdrawButton
+      <Dialog
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle>회원 탈퇴</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            정말로 회원 탈퇴를 진행하시겠습니까? 이 작업은 되돌릴 수 없습니다.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <CustomCancelButton onClick={handleClose}>
+            취소
+          </CustomCancelButton>
+          <CustomWithdrawButton onClick={handleWithdraw}>
+            탈퇴
+          </CustomWithdrawButton>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default WithdrawButton;
